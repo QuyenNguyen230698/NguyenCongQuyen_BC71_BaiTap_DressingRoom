76519693352af05d324061c99c0d2ce33daee83f
QuyenNguyen230698 <166916954+QuyenNguyen230698@users.noreply.github.com>
2024-08-14 22:06:41 +0700

    update layout Quyen

---
+++ dressingroom/controllers/main.js
@@ -0,0 +1,403 @@
+
+// ÁO
+function layoutAo() {
+    var string = `
+    <div class="row">
+    <div class="card">
+      <img
+        style="width: 100%"
+        src="/dressingroom/assets/images/clothes/topcloth1_show.jpg"
+        alt=""
+      />
+      <div class="card-body text-center">
+        <h4 class="card-text">Top Cloth 1</h4>
+        <button onclick="topCloth1()" class="btn btn-primary">Thử đồ</button>
+      </div>
+    </div>
+    <div class="card">
+      <img
+        style="width: 100%"
+        src="/dressingroom/assets/images/clothes/topcloth2_show.jpg"
+        alt=""
+      />
+      <div class="card-body text-center">
+        <h4 class="card-text">Top Cloth 2</h4>
+        <button class="btn btn-primary">Thử đồ</button>
+      </div>
+    </div>
+    <div class="card">
+      <img
+        style="width: 100%"
+        src="/dressingroom/assets/images/clothes/topcloth3_show.jpg"
+        alt=""
+      />
+      <div class="card-body text-center">
+        <h4 class="card-text">Top Cloth 3</h4>
+        <button class="btn btn-primary">Thử đồ</button>
+      </div>
+    </div>
+    <div class="card">
+      <img
+        style="width: 100%"
+        src="/dressingroom/assets/images/clothes/topcloth4_show.jpg"
+        alt=""
+      />
+      <div class="card-body text-center">
+        <h4 class="card-text">Top Cloth 4</h4>
+        <button class="btn btn-primary">Thử đồ</button>
+      </div>
+    </div>
+    <div class="card">
+      <img
+        style="width: 100%"
+        src="/dressingroom/assets/images/clothes/topcloth5_show.jpg"
+        alt=""
+      />
+      <div class="card-body text-center">
+        <h4 class="card-text">Top Cloth 5</h4>
+        <button class="btn btn-primary">Thử đồ</button>
+      </div>
+    </div>
+    <div class="card">
+      <img
+        style="width: 100%"
+        src="/dressingroom/assets/images/clothes/topcloth6_show.jpg"
+        alt=""
+      />
+      <div class="card-body text-center">
+        <h4 class="card-text">Top Cloth 6</h4>
+        <button class="btn btn-primary">Thử đồ</button>
+      </div>
+    </div>
+  </div>`;
+    document.getElementById('layoutSP').innerHTML = string;
+}
+layoutAo()
+//QUẦN
+document.getElementById('spQuan').onclick = function() {
+    var string = `
+    <div class="row">
+    <div class="card">
+      <img
+        style="width: 100%"
+        src="/dressingroom/assets/images/clothes/botcloth1_show.jpg"
+        alt=""
+      />
+      <div class="card-body text-center">
+        <h4 class="card-text">Bot Cloth 1</h4>
+        <button class="btn btn-primary">Thử đồ</button>
+      </div>
+    </div>
+    <div class="card">
+      <img
+        style="width: 100%"
+        src="/dressingroom/assets/images/clothes/botcloth2_show.jpg"
+        alt=""
+      />
+      <div class="card-body text-center">
+        <h4 class="card-text">Bot Cloth 2</h4>
+        <button class="btn btn-primary">Thử đồ</button>
+      </div>
+    </div>
+    <div class="card">
+      <img
+        style="width: 100%"
+        src="/dressingroom/assets/images/clothes/botcloth3_show.jpg"
+        alt=""
+      />
+      <div class="card-body text-center">
+        <h4 class="card-text">Bot Cloth 3</h4>
+        <button class="btn btn-primary">Thử đồ</button>
+      </div>
+    </div>
+    <div class="card">
+      <img
+        style="width: 100%"
+        src="/dressingroom/assets/images/clothes/botcloth4_show.jpg"
+        alt=""
+      />
+      <div class="card-body text-center">
+        <h4 class="card-text">Bot Cloth 4</h4>
+        <button class="btn btn-primary">Thử đồ</button>
+      </div>
+    </div>
+    <div class="card">
+      <img
+        style="width: 100%"
+        src="/dressingroom/assets/images/clothes/botcloth5_show.jpg"
+        alt=""
+      />
+      <div class="card-body text-center">
+        <h4 class="card-text">Bot Cloth 5</h4>
+        <button class="btn btn-primary">Thử đồ</button>
+      </div>
+    </div>
+  </div>`;
+    document.getElementById('layoutSP').innerHTML = string;
+}
+// GIÀY DÉP
+document.getElementById('spGiay').onclick = function() {
+    var string = `
+    <div class="row">
+    <div class="card">
+      <img
+        style="width: 100%"
+        src="/dressingroom/assets/images/shoes/shoes1_show.jpg"
+        alt=""
+      />
+      <div class="card-body text-center">
+        <h4 class="card-text">Shoes 1</h4>
+        <button class="btn btn-primary">Thử đồ</button>
+      </div>
+    </div>
+    <div class="card">
+      <img
+        style="width: 100%"
+        src="/dressingroom/assets/images/shoes/shoes2_show.jpg"
+        alt=""
+      />
+      <div class="card-body text-center">
+        <h4 class="card-text">Shoes 2</h4>
+        <button class="btn btn-primary">Thử đồ</button>
+      </div>
+    </div>
+    <div class="card">
+      <img
+        style="width: 100%"
+        src="/dressingroom/assets/images/shoes/shoes3_show.jpg"
+        alt=""
+      />
+      <div class="card-body text-center">
+        <h4 class="card-text">Shoes 3</h4>
+        <button class="btn btn-primary">Thử đồ</button>
+      </div>
+    </div>
+    <div class="card">
+      <img
+        style="width: 100%"
+        src="/dressingroom/assets/images/shoes/shoes4_show.jpg"
+        alt=""
+      />
+      <div class="card-body text-center">
+        <h4 class="card-text">Shoes 4</h4>
+        <button class="btn btn-primary">Thử đồ</button>
+      </div>
+    </div>
+    <div class="card">
+      <img
+        style="width: 100%"
+        src="/dressingroom/assets/images/shoes/shoes5_show.jpg"
+        alt=""
+      />
+      <div class="card-body text-center">
+        <h4 class="card-text">Shoes 5</h4>
+        <button class="btn btn-primary">Thử đồ</button>
+      </div>
+    </div>
+  </div>`;
+    document.getElementById('layoutSP').innerHTML = string;
+}
+// TÚI XÁCH
+document.getElementById('spTui').onclick = function() {
+    var string = `
+    <div class="row">
+    <div class="card">
+      <img
+        style="width: 100%"
+        src="/dressingroom/assets/images/handbags/handbag1_show.jpg"
+        alt=""
+      />
+      <div class="card-body text-center">
+        <h4 class="card-text">handbags 1</h4>
+        <button class="btn btn-primary">Thử đồ</button>
+      </div>
+    </div>
+    <div class="card">
+      <img
+        style="width: 100%"
+        src="/dressingroom/assets/images/handbags/handbag2_show.jpg"
+        alt=""
+      />
+      <div class="card-body text-center">
+        <h4 class="card-text">handbags 2</h4>
+        <button class="btn btn-primary">Thử đồ</button>
+      </div>
+    </div>
+    <div class="card">
+      <img
+        style="width: 100%"
+        src="/dressingroom/assets/images/handbags/handbag3_show.jpg"
+        alt=""
+      />
+      <div class="card-body text-center">
+        <h4 class="card-text">handbags 3</h4>
+        <button class="btn btn-primary">Thử đồ</button>
+      </div>
+    </div>
+  </div>`;
+  document.getElementById('layoutSP').innerHTML = string;
+}
+//DÂY CHUYỀN
+document.getElementById('spDay').onclick = function() {
+    var string = `
+    <div class="row">
+    <div class="card">
+      <img
+        style="width: 100%"
+        src="/dressingroom/assets/images/necklaces/necklace1_show.jpg"
+        alt=""
+      />
+      <div class="card-body text-center">
+        <h4 class="card-text">necklaces 1</h4>
+        <button class="btn btn-primary">Thử đồ</button>
+      </div>
+    </div>
+    <div class="card">
+      <img
+        style="width: 100%"
+        src="/dressingroom/assets/images/necklaces/necklace2_show.jpg"
+        alt=""
+      />
+      <div class="card-body text-center">
+        <h4 class="card-text">necklaces 2</h4>
+        <button class="btn btn-primary">Thử đồ</button>
+      </div>
+    </div>
+    <div class="card">
+      <img
+        style="width: 100%"
+        src="/dressingroom/assets/images/necklaces/necklace3_show.jpg"
+        alt=""
+      />
+      <div class="card-body text-center">
+        <h4 class="card-text">necklaces 3</h4>
+        <button class="btn btn-primary">Thử đồ</button>
+      </div>
+    </div>
+  </div>`;
+    document.getElementById('layoutSP').innerHTML = string;
+}
+//KIỂU TÓC
+document.getElementById('spToc').onclick = function() {
+    var string = `
+    <div class="row">
+    <div class="card">
+      <img
+        style="width: 100%"
+        src="/dressingroom/assets/images/hairstyle/hairstyle1_show.jpg"
+        alt=""
+      />
+      <div class="card-body text-center">
+        <h4 class="card-text">hairstyle 1</h4>
+        <button class="btn btn-primary">Thử đồ</button>
+      </div>
+    </div>
+    <div class="card">
+      <img
+        style="width: 100%"
+        src="/dressingroom/assets/images/hairstyle/hairstyle2_show.jpg"
+        alt=""
+      />
+      <div class="card-body text-center">
+        <h4 class="card-text">hairstyle 2</h4>
+        <button class="btn btn-primary">Thử đồ</button>
+      </div>
+    </div>
+    <div class="card">
+      <img
+        style="width: 100%"
+        src="/dressingroom/assets/images/hairstyle/hairstyle3_show.jpg"
+        alt=""
+      />
+      <div class="card-body text-center">
+        <h4 class="card-text">hairstyle 3</h4>
+        <button class="btn btn-primary">Thử đồ</button>
+      </div>
+    </div>
+  </div>`;
+    document.getElementById('layoutSP').innerHTML = string;
+}
+//NỀN
+document.getElementById('spNen').onclick = function() {
+    var string = `
+    <div class="row">
+    <div class="card">
+      <img
+        style="width: 176;height: 208px;"
+        src="/dressingroom/assets/images/background/background1_show.jpg"
+        alt=""
+      />
+      <div class="card-body text-center">
+        <h4 class="card-text">Background 1</h4>
+        <button onclick="background1()" class="btn btn-primary">Thử đồ</button>
+      </div>
+    </div>
+    <div class="card">
+      <img
+      style="width: 176;height: 208px;"
+        src="/dressingroom/assets/images/background/background2_show.jpg"
+        alt=""
+      />
+      <div class="card-body text-center">
+        <h4 class="card-text">Background 2</h4>
+        <button class="btn btn-primary">Thử đồ</button>
+      </div>
+    </div>
+    <div class="card">
+      <img
+      style="width: 176;height: 208px;"
+        src="/dressingroom/assets/images/background/background3_show.jpg"
+        alt=""
+      />
+      <div class="card-body text-center">
+        <h4 class="card-text">Background 3</h4>
+        <button class="btn btn-primary">Thử đồ</button>
+      </div>
+    </div>
+    <div class="card">
+      <img
+      style="width: 176;height: 208px;"
+        src="/dressingroom/assets/images/background/background4_show.jpg"
+        alt=""
+      />
+      <div class="card-body text-center">
+        <h4 class="card-text">Background 4</h4>
+        <button class="btn btn-primary">Thử đồ</button>
+      </div>
+    </div>
+    <div class="card">
+      <img
+      style="width: 176;height: 208px;"
+        src="/dressingroom/assets/images/background/background5_show.jpg"
+        alt=""
+      />
+      <div class="card-body text-center">
+        <h4 class="card-text">Background 5</h4>
+        <button class="btn btn-primary">Thử đồ</button>
+      </div>
+    </div>
+    <div class="card">
+      <img
+      style="width: 176;height: 208px;"
+        src="/dressingroom/assets/images/background/background6_show.jpg"
+        alt=""
+      />
+      <div class="card-body text-center">
+        <h4 class="card-text">Background 6</h4>
+        <button class="btn btn-primary">Thử đồ</button>
+      </div>
+    </div>
+    <div class="card">
+      <img
+      style="width: 176;height: 208px;"
+        src="/dressingroom/assets/images/background/background7_show.jpg"
+        alt=""
+      />
+      <div class="card-body text-center">
+        <h4 class="card-text">Background 7</h4>
+        <button class="btn btn-primary">Thử đồ</button>
+      </div>
+    </div>
+  </div>`;
+    document.getElementById('layoutSP').innerHTML = string;
+}
---
+++ dressingroom/models/ChoseItem.js
@@ -0,0 +1,10 @@
+
+ function topCloth1() {
+    console.log('hello');
+}
+
+function background1() {
+    console.log('success');
+    const change = document.getElementById('bgLayout')
+    change.style = "background-image: url(/dressingroom/assets/images/background/background2.jpg)"
+}
